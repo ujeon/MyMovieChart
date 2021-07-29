9f91d48a55a12ba8982bd942da25b3c7aa35fe22
ujeon_jang <wj.jang@newploy.net>
2021-07-30 08:26:22 +0900

    테이블 뷰를 활용하여 데이터 목록 구현

---
+++ MyMovieChart/ListViewController.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class ListViewController: UITableViewController {
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-17", 9.19)
+    ]
+    
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        
+        return datalist
+    }()
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = self.list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        cell.detailTextLabel?.text = row.description
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
+    }
+}
---
+++ MyMovieChart/MovieVO.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class MovieVO {
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var detail: String?
+    var opendate: String?
+    var rating: Double?
+}
