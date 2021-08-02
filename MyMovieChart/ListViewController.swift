d6ca6fe9c0f63bcd6283fe4363ff01d345e6c614
ujeon_jang <wj.jang@newploy.net>
2021-08-02 14:53:13 +0900

    iOS에서 API 호출하기

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -1,27 +1,16 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-    var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darkknight.jpeg"),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpeg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-17", 9.19, "secret.gif")
-    ]
-    
+    var page: Int = 1
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumbnail = thumbnail
-            
-            datalist.append(mvo)
-        }
-        
         return datalist
     }()
+    @IBOutlet var moreBtn: UIButton!
+    
+    override func viewDidLoad() {
+        self.callMovieAPI()
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.list.count
@@ -30,17 +19,59 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = self.list[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
+        let url: URL! = URL(string: row.thumbnail!)
+        let imageData = try! Data(contentsOf: url)
         
         cell.title.text = row.title
         cell.desc.text = row.description
         cell.opendate.text = row.opendate
         cell.rating.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        cell.thumbnail.image = UIImage(data: imageData)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
+    }
+    
+    @IBAction func more(_ sender: Any) {
+        self.page += 1
+        
+        self.callMovieAPI()
+        self.tableView.reloadData()
+    }
+    
+    func callMovieAPI() {
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        
+        do {
+            let apidata = try Data(contentsOf: apiURI)
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+            
+            for row in movie {
+                let r = row as! NSDictionary
+                let mvo = MovieVO()
+                
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                self.list.append(mvo)
+            }
+            
+            if self.list.count >= totalCount {
+                self.moreBtn.isHidden = true
+            }
+        } catch {
+            NSLog("오류 발생!")
+        }
     }
 }
